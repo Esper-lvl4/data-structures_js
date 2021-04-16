590438ae93a8f36f07a2b66effe9cccc3a69b0c7
Esper_lvl4 <inventoris7@gmail.com>
2021-04-16 09:36:35 +0300

    Queue not does not store size inside itself - it instead uses getter to get it's size from DoubleLinkedList.

--- structures/Queue.ts
+++ structures/Queue.ts
@@ -2,24 +2,23 @@ import DoublyLinkedList from "./DoublyLinkedList.ts";
 
 export class Queue<T> {
   list: DoublyLinkedList<T>;
-  size: number;
   constructor() {
-    this.size = 0;
     this.list = new DoublyLinkedList();
   }
   enqueue(value: T) {
     this.list.add(value);
-    this.size += 1;
   }
   dequeue(): T | undefined {
     if (!this.list.head) return;
     const value = this.list.head.value;
     this.list.remove(value);
-    this.size -= 1;
     return value;
   }
   get front(): T | undefined {
     return this.list.head?.value;
+  }
+  get size(): number {
+    return this.list.size;
   }
 }
 
